191e6e127b71a0a10c0fcff1120cdd8bdf6eb009
tiartrop <90901624+tiartrop@users.noreply.github.com>
2025-01-21 14:14:04 +0800

    chore: update

--- bilibili/src/assets/trackParams.js
+++ bilibili/src/assets/trackParams.js
@@ -1,6 +1,8 @@
 // 参考 https://greasyfork.org/scripts/471831
 const trackUrlParams = [
+  'broadcast_type',
   'buvid',
+  'from_source',
   'from_spmid',
   'is_room_feed',
   'is_story_h5',
@@ -8,7 +10,7 @@ const trackUrlParams = [
   'liteVersion',
   'live_from',
   'mid',
-  'broadcast_type',
+  'search_source',
   'session_id',
   'timestamp',
   'up_id',
